005217ad738053b443a9d3ec5eb29ed0a0b5989d
dayeon <dayeon2399@naver.com>
2020-12-07 19:22:06 +0900

    dayeon's commit - done done:)

--- app/src/main/java/org/techtown/mirimgridviewtest/GridAdapter.java
+++ app/src/main/java/org/techtown/mirimgridviewtest/GridAdapter.java
@@ -21,7 +21,7 @@ public class GridAdapter  extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return imgRes.length;
     }
 
     @Override
--- app/src/main/java/org/techtown/mirimgridviewtest/MainActivity.java
+++ app/src/main/java/org/techtown/mirimgridviewtest/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
-
+    GridView gridv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
         setTitle("영화포스터 목록");
-        GridView gridv=findViewById(R.id.gridv);
+        gridv=findViewById(R.id.gridv);
         GridAdapter adapter=new GridAdapter(this);
         gridv.setAdapter(adapter);
     }
